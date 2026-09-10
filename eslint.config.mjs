f54a685dcc56f0f4aaf1edc003745e5c80635be4
Suraj Saikia <2005surajsaikia@gmail.com>
2026-09-10 19:19:15 +0530

    Added GitHub Actions CI

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.node,
+        ...globals.jest // Adds support for test() and expect()
+      } 
+    } 
+  },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { sourceType: "commonjs" } 
+  },
+]);
---
+++ test/index.test.js
@@ -0,0 +1,4 @@
+const add = require("../index");
+test("adds 5 + 10", () => {
+  expect(add(5, 10)).toBe(15);
+});
